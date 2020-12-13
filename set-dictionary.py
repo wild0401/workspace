3260ef6730875061948981988e36c2e5ba6bda8b
wild0401 <75138313+wild0401@users.noreply.github.com>
2020-12-13 21:52:33 +0800

    Add files via upload

---
+++ set-dictionary.py
@@ -0,0 +1,22 @@
+#集合的運算
+#s1={3,4,5}
+#print(10 not in s1)
+#s1={3,4,5}
+#s2={4,5,6,7}
+#s3=s1&s2 #交集:取兩個集合中，相同的資料
+#s3=s1|s2 #聯集:取兩個集合中的所有資料，但不重複取
+#s3=s1-s2 #差集:從s1中，減去和s2重疊的部分
+#s3=s1^s2 #反交集:取兩個集合中不重疊的部分
+#print(s3)
+#s=set("Hello")#把字串中的字母拆解成集合:set(字串)
+#print("A" in s)
+#字典的運算
+#dic={"apple":"蘋果","bug":"蟲蟲"}
+#print(dic)
+#del dic["apple"]    #刪除字典中的鍵值對(key-value pair)
+#print(dic)
+#dic["apple"]="小蘋果"
+#print(dic["apple"])
+#print("test" not in dic)   #判斷key是否存在
+dic={x:x*2 for x in [3,4,5]}    #從列表的資料產生字典
+print(dic)
